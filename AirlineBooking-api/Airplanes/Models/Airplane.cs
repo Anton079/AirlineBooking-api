aac228c09f65dc5e69966373744447ef44df1d18
HoriaAnton <horiaantonio@gmail.com>
2025-06-30 20:14:33 +0300

    first commit

---
+++ AirlineBooking-api/Airplanes/Models/Airplane.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirlineBooking_api.Airplanes.Models
+{
+    [Table("airplane")]
+    public class Airplane
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("bookingId")]
+        public int Id { get; set; }
+
+
+
+
+    }
+}
---
+++ AirlineBooking-api/Tickets/Models/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirlineBooking_api.Tickets.Models
+{
+    [Table("ticket")]
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("customerId")]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Column("description")]
+        public string Description { get; set; }
+    }
+}
